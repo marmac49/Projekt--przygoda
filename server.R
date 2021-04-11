9a3ddc3a421da0288db151dc4376dc10de6099c8
marmac49 <81258320+marmac49@users.noreply.github.com>
2021-04-12 00:08:04 +0200

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,31 @@
-shinyServer(function(input, output) {
-})
-
-
+shinyServer(function(input, output,session){
+  
+  
+  output$wybrane_pytanie  <- renderText({input$pytania})
+  output$udzielona_odpowiedz <- renderText({input$pytanie})
+  output$odp <- renderUI({
+    
+    switch(input$pytania,
+           "Pytanie 1" = checkboxGroupInput("pytanie", "Jak miał na nazwisko główny bohater Pana Tadeusza",
+                                            choices = c("Rejent","Podkomorzy", "Spolica", "nie było wspomniane"),
+                                            selected = "option2"),
+           "Pytanie 2" = checkboxGroupInput("pytanie", "Kto napisał Harrego Pottera?",
+                                            choices = c("J.K.Rowling","Olga Tokarczuk","Stephen King", "Nicholas Sparks"),
+                                            selected = "option2"),
+           "Pytanie 3" = checkboxGroupInput("pytanie", "Której książki nie napisał Stephen King?",
+                                            choices = c("To","Lśnienie", "Carrie", "Szczęki"),
+                                            selected = "option2")
+           
+    )
+    
+  })
+  
+  
+  
+  
+  observe({
+    if (input$pytania != "Wybierz") {
+      updateCollapse(session,"collapser",open = c('rozwiń/zwiń'))
+      
+    }
+  })})
